a1de05a0729f914dfb7b7c640e79061396a292f8
kamran-hussen <kamran.techydevs@gmail.com>
2023-03-15 15:34:38 +0600

    hello

---
+++ src/features/auth/authApiSlice.js
@@ -0,0 +1,8 @@
+import { apiSlice } from "../../app/api/apiSlice";
+import { logOut } from "./authSlice";
+
+export const authApiSlice = apiSlice.injectEndpoints({
+    endpoints: builder => ({
+        
+    })
+})
